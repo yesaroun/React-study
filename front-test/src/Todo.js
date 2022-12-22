fd86656fe7f7c8fa9eb440576fed926473a1f248
akor11lyt <akor1@naver.com>
2022-12-22 16:26:42 +0900

    todo api

--- front-test/src/Todo.js
+++ front-test/src/Todo.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import TodoItem from "./TodoItem";
 
 function Todo() {
@@ -6,31 +6,41 @@ function Todo() {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
 
-  const onChange = (event) => {
-    console.log(event.target.value);
-    setTodo(event.target.value);
-  };
+  // const onChange = (event) => {
+  //   console.log(event.target.value);
+  //   setTodo(event.target.value);
+  // };
+  //
+  // const onSubmit = (event) => {
+  //   event.preventDefault(); // 새로고침 막아줌
+  //   console.log("onSubmit Click");
+  //
+  //   setTodos((currentTodos) => [todo, ...currentTodos]);
+  //   setTodo("");  // 초기화 시킨다.
+  //
+  //   console.log(todos);
+  // };
 
-  const onSubmit = (event) => {
-    event.preventDefault(); // 새로고침 막아줌
-    console.log("onSubmit Click");
-
-    setTodos((currentTodos) => [todo, ...currentTodos]);
-    setTodo("");  // 초기화 시킨다.
-
-    console.log(todos);
-  };
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setTodos(data);
+      });
+  });
 
   return (<div>
-      <form onSubmit={onSubmit} action="">
-        <input value={todo} onChange={onChange} type="text" placeholder="할 일을 추가하세요."/>
-        <button>Add todo</button>
-      </form>
-      {todos.map((item, index) => {
-        return <TodoItem index={index} item={item}></TodoItem>
-        // return <li key={index}>{item}</li>;
-      })}
-    </div>);
+    {/*<form onSubmit={onSubmit} action="">*/}
+    {/*  <input value={todo} onChange={onChange} type="text" placeholder="할 일을 추가하세요."/>*/}
+    {/*  <button>Add todo</button>*/}
+    {/*</form>*/}
+    {todos.map((item, index) => {
+      // return <TodoItem index={index} item={item}></TodoItem>
+      // return <li key={index}>{item}</li>;
+      return <TodoItem key={item.id} name={item.name} email={item.email}></TodoItem>
+    })}
+
+  </div>);
 }
 
 export default Todo;
--- front-test/src/TodoItem.js
+++ front-test/src/TodoItem.js
@@ -4,15 +4,23 @@ import moment from "moment";      // 시간 모듈
 // function TodoItem(props) {
 //    console.log("props : ", props); 이렇게 확인 가능
 // {props.index} 이렇게 사용 가능
-function TodoItem({index, item}) {
-  // const [time, setTime] = useState("");
-  const  nowTime = moment().format("YYYY-MM-DD HH:mm:ss");
+// function TodoItem({index, item}) {
+//   // const [time, setTime] = useState("");
+//   const  nowTime = moment().format("YYYY-MM-DD HH:mm:ss");
+//
+//   return (
+//     <div className={styles.todoItem}>
+//       <h1>{index+1} 번째 리스트</h1>
+//       <li key={index}>{item}</li>
+//       <p>작성 시간: {nowTime}</p>
+//     </div>
+//   );
+// }
 
+function TodoItem({name, email}) {
   return (
-    <div className={styles.todoItem}>
-      <h1>{index+1} 번째 리스트</h1>
-      <li key={index}>{item}</li>
-      <p>작성 시간: {nowTime}</p>
+    <div>
+      <li>{name} / {email}</li>
     </div>
   );
 }
