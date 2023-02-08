c3718652afd84f04dfb1dd1180c5a7b50354e459
yesaroun <akor1@naver.com>
2023-02-08 13:23:23 +0900

    fbase 세팅

---
+++ nwitter-front-react/nwitter/src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDcDOHCS-931vlWPR8UH_wy2yu9KekUvdw",
+  authDomain: "nwitter-5baf6.firebaseapp.com",
+  projectId: "nwitter-5baf6",
+  storageBucket: "nwitter-5baf6.appspot.com",
+  messagingSenderId: "317410921354",
+  appId: "1:317410921354:web:17ea85fa9d4c5aa4f85673"
+};
+
+export default firebase.initializeApp(firebaseConfig);
