67a345fd9e8c78d3749e48561a61702474ec884e
yesaroun <akor1@naver.com>
2023-02-12 05:37:20 +0900

    feet: random score

--- Doosimi-Test-master-temp/src/routes/Result.js
+++ Doosimi-Test-master-temp/src/routes/Result.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import RandomScore from './RandomScore';
 
 const Result = () => {
-  return <div>hello</div>;
+  return (
+    <>
+      <RandomScore />
+      <div>hi</div>
+    </>
+  );
 };
 
 export default Result;
