13e5a56c5adb27c7824f885b5d50c5f0d1651cba
yesaroun <akor1@naver.com>
2023-02-12 04:24:53 +0900

    project 세팅

---
+++ Doosimi-Test-master-temp/README.md
@@ -0,0 +1,1 @@
+# Doosimi-test Project
--- movie_app-front-react/movie_app/src/App.js
+++ movie_app-front-react/movie_app/src/App.js
@@ -1,52 +1,17 @@
-import React from 'react';
-import PropTypes from "prop-types";
-
-function Food({name, picture, rating}) {
-  return (
-    <div>
-      <h1>I like { name }</h1>
-      <h4>{rating}/5.0</h4>
-      <img src={picture}/>
-    </div>
-  );
-}
-
-const foodILike = [
-  {
-    id: 1,
-    name: 'Kimchi',
-    rating: 5.0,
-    image: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbfU91C%2FbtrroyqLD6T%2FDjcxlQNMDVU42HdICxYL0k%2Fimg.png',
-  },
-  {
-    id: 2,
-    name: 'Samgyeopsal',
-    rating: 4.0,
-    image: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcFVqaE%2FbtrG8HlkyHS%2FbqKtQJCC3sZq5BRcxkuzEk%2Fimg.png'
-  },
-  {
-    id: 3,
-    name: 'Bibimbap',
-    rating: 3.0,
-    image: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fl0FRm%2FbtrFHdMBEMG%2F0M8b34EgS1HNz368iYChjk%2Fimg.png'
-  }
-];
+import React from "react";
+import './App.css';
+import {HashRouter, Route, Routes} from "react-router-dom";
+import About from './routes/About';
 
 function App() {
   return (
-    <div>
-      {/*{foodILike.map(renderFood)}*/}
-      {foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
-      ))}
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route path="/about" element={<About />}/>
+
+      </Routes>
+    </HashRouter>
   );
 }
-
-Food.prototype = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
 
 export default App;
